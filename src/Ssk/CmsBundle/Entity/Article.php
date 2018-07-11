42974d0c5b0640b8d068b6966595a79b36309461
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2018-07-11 12:54:41 +0200

    Wygenerowanie getters and setters za pomoca doctrine

--- src/Ssk/CmsBundle/Entity/Article.php
+++ src/Ssk/CmsBundle/Entity/Article.php
@@ -2,14 +2,114 @@
 
 namespace Ssk\CmsBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="article")
+ */
 class Article{
 
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     protected $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
     protected $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
     protected $created;
 
-    
-}
 
-?>
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Article
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Article
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
