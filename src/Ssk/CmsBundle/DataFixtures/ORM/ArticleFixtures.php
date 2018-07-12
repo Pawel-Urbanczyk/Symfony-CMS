ab07293fff4df6eedfa3f99ab2d2bec0ff10534b
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2018-07-12 11:13:46 +0200

    Doinstalowanie pakietow Doctrine, utworzenie DataFixtures dla Articles oraz dopisanie odpowiednich klas

---
+++ src/Ssk/CmsBundle/DataFixtures/ORM/ArticleFixtures.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ssk\CmsBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Ssk\CmsBundle\Entity\Article;
+
+
+?>
